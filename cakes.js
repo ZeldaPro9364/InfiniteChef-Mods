ad93706c721128e1fdea7c7940ca64069a6d8abc
ZeldaPro9364 <105319836+ZeldaPro9364@users.noreply.github.com>
2026-01-29 15:48:01 -0800

    cakes.js

---
+++ cakes.js
@@ -0,0 +1,15 @@
+addIngredient("Chocolate cake",{
+    color:"#964B00",
+    type:"cake"
+});
+
+addIngredient("Strawberry cake",{
+    color:"#FF0000",
+    type:"cake"
+});
+
+addIngredient("Vanilla cake",{
+    color:"#FFFDD0",
+    type:"cake"
+});
+
